8b052d7588ed22c9e80f476e6c982633eddf4938
vakas-786 <vakas0959@gmail.com>
2020-08-25 10:37:17 -0400

    final

--- src/Components/DogCard.js
+++ src/Components/DogCard.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
 
-function DogCard() {
+class DogCard extends Component {
+
+state = {text: "Ruff"}
+  // clickHandler =()=>{
+
+  //   console.log("clicked")
+  // }
+  clickHandler =()=>{  
+    
+   }
+
+  // const buttonToggle = {
+  //    if (clickHandler){
+  //     <h2></h2>
+  //    }
+  // }
+  render() {
   return (
+
     <div>
-      <h2 >{/*Dog name goes here*/}</h2>
-      <img alt="" src={""} />
-      <button className="bark">Bark</button>
+      <h2 >{this.props.dogName}</h2>
+      <img alt="" src={this.props.dogImage} />
+      <button onClick={this.clickHandler} className="bark">Bark</button>
     </div>
   );
+  }
 }
 
 export default DogCard;
--- src/Containers/DogsList.js
+++ src/Containers/DogsList.js
@@ -1,8 +1,20 @@
 import React, { Component } from "react";
+import DogCard from "../Components/DogCard"
+import {apiResponse} from "../api"
 
 class DogList extends Component {
+
+  
+  
   render() {
-    return <div className="dogContainer">{/*list of dogs go here*/}</div>;
+   let fetchDog = apiResponse.map((dogObj) => <DogCard dogImage={dogObj.img} dogName={dogObj.name}/> ) 
+
+    // console.log(apiResponse)
+    return <div className="dogContainer">
+      <p>{fetchDog}</p>
+      <DogCard />
+      
+      </div>;
   }
 }
 

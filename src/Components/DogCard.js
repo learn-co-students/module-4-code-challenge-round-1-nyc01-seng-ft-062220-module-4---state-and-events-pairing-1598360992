e14186698f4d154108a66e0ee08e807904e710a0
scubaguba345 <brandon.gubitosa@gmail.com>
2020-08-25 10:09:58 -0400

    done

--- src/Components/DogCard.js
+++ src/Components/DogCard.js
@@ -1,13 +1,49 @@
 import React from "react";
 
-function DogCard() {
-  return (
-    <div>
-      <h2 >{/*Dog name goes here*/}</h2>
-      <img alt="" src={""} />
-      <button className="bark">Bark</button>
-    </div>
-  );
+
+
+class DogCard extends React.Component {
+
+  constructor(){
+    super()
+    this.state = {
+      button: "Bark"
+    }
+  }
+  
+
+
+  clickHandler = () => {
+    if (this.state.button === "Bark"){
+      this.setState({
+        button: "Ruff"
+      })
+    }else{
+    this.setState({
+      button: "Bark"
+    })
+  }
 }
 
+  render() {
+
+    return (
+    
+      <div>
+        <h2 >{this.props.dogInfo.name}</h2>
+        <img alt={this.props.dogInfo.name} src={this.props.dogInfo.img} />
+        <button onClick={this.clickHandler} className="bark">Bark</button>
+        
+        <h2>{this.state.button === "Bark" ? null : "Ruff"}</h2>
+      </div>
+    );
+  }
+
+  }
+
+
+
 export default DogCard;
+
+//When a User clicks on the "Bark" button it should render an <h2> to the screen. The <h2> innerText should say "Ruff" and its class should be "bark"
+// When a User clicks on the button a second time the "Ruff" should be removed from the DOM
--- src/Containers/DogsList.js
+++ src/Containers/DogsList.js
@@ -1,9 +1,25 @@
 import React, { Component } from "react";
+import {apiResponse} from '../api.js'
+import DogCard from "../Components/DogCard"
 
 class DogList extends Component {
+
+
+    renderDogs = () => {
+      return (
+        apiResponse.map((dog, index) => <DogCard dogInfo = {dog} key={index} />)
+      )
+    }
+
+
+
   render() {
-    return <div className="dogContainer">{/*list of dogs go here*/}</div>;
+
+  
+    return <div className="dogContainer">{this.renderDogs()}</div>;
   }
 }
 
 export default DogList;
+
+
